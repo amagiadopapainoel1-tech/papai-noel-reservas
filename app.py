dd35bb01a316bf06b6ab04dd38f8cc70ad06f7fb
amagiadopapainoel1-tech <amagiadopapainoel1@gmail.com>
2025-11-29 17:12:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,57 +1,4 @@
-from flask import Flask, render_template, request, jsonify
-import os
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-import json
-from datetime import datetime, timedelta
-
-app = Flask(__name__)
-
-# ====== AUTENTICAÇÃO GOOGLE VIA VARIÁVEL DO RENDER ======
-google_credentials_json = os.getenv("GOOGLE_CREDENTIALS")
-if not google_credentials_json:
-    raise Exception("ERRO: A variável GOOGLE_CREDENTIALS não está definida no Render!")
-
-creds_dict = json.loads(google_credentials_json)
-
-scope = [
-    "https://spreadsheets.google.com/feeds",
-    "https://www.googleapis.com/auth/drive"
-]
-
-creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
-client = gspread.authorize(creds)
-
-# === SUA PLANILHA ===
-SHEET_ID = "10HFxgC2k_6VkFyUoTiPMg6h0aBUhUaESYg5knbapktM"
-sheet = client.open_by_key(SHEET_ID).sheet1
-
-
-# ==========================================================
-# FUNÇÃO PARA GERAR HORÁRIOS DE 2 EM 2 MINUTOS
-# ==========================================================
-def gerar_horarios(data):
-    if data == "2024-12-24":
-        inicio = datetime(2024, 12, 24, 14, 0)
-        fim = datetime(2024, 12, 24, 23, 59)
-
-    elif data == "2024-12-25":
-        inicio = datetime(2024, 12, 25, 0, 0)
-        fim = datetime(2024, 12, 25, 11, 0)
-
-    else:
-        return []
-
-    horarios = []
-    atual = inicio
-
-    while atual <= fim:
-        horarios.append(atual.strftime("%H:%M"))
-        atual += timedelta(minutes=2)
-
-    return horarios
-
-
+v
 # ==========================================================
 # ROTA PRINCIPAL — CARREGA O FORMULÁRIO
 # ==========================================================
@@ -114,4 +61,5 @@ def enviar():
 # ==========================================================
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
+
 
